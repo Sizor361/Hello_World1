feec891fc92600aada14b1a6623d51c812a7ffe6
Sizor361 <142895529+Sizor361@users.noreply.github.com>
2024-07-29 19:15:16 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; //Это код который подтягивает извне
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start() //Всё что находится внутри фигурных скобках здесь исполняется 1 раз при запуске игры
+    {
+        print("Hello World!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
